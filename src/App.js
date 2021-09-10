0bfe09d4de244351d3ff67dfb60695f4119f8667
Alemarnery <aleemar.95@gmail.com>
2021-09-09 21:23:22 -0400

    header

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Components/Header";
+function App() {
+  return (
+    <>
+      <Header titulo="Clima" />
+    </>
+  );
+}
+
+export default App;
